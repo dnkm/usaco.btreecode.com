829b64ded82ecd084c53904ee6c185c5bca1a7a3
Sqenge <21sqen@gmail.com>
2022-01-18 15:49:34 -0800

    admin student search

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,10 +4,11 @@ import {
   doc,
   getDoc,
   getDocs,
+  loadBundle,
   query,
   where,
 } from "firebase/firestore";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AppContext from "../util/context";
 import { auth, fstore } from "../util/fire";
@@ -16,31 +17,39 @@ import List from "./List/Index";
 export default function Admin() {
   const { user, userData } = useContext(AppContext);
   let navigate = useNavigate();
+  let [active, setActive] = useState([]);
   let [students, setStudents] = useState([]);
 
   useEffect(() => {
-    if (userData) {
-      if (userData.isAdmin) {
-        loadStudents();
-      } else {
-        navigate(`/`);
-      }
+    if (!userData.isAdmin) {
+      navigate(`/`);
     }
+    loadStudents();
   }, [user, userData]);
+
+  function setStudent(stdId) {
+    navigate("/?id=" + stdId);
+  }
 
   async function loadStudents() {
     let ref = collection(fstore, "user_data");
     let q = query(ref, where("isActive", "==", true));
     let { docs } = await getDocs(q);
+    setActive(docs);
     setStudents(docs);
   }
 
-  function setStudent(stdId) {
-    navigate("/?id=" + stdId);
-  }
   function search(ev) {
     ev.preventDefault();
-
+    setActive(
+      students.filter(
+        (st) =>
+          st
+            .data()
+            .name.toLowerCase()
+            .indexOf(ev.target.search.value.toLowerCase()) > -1
+      )
+    );
     ev.target.reset();
   }
 
@@ -48,14 +57,22 @@ export default function Admin() {
     <div className="flex flex-col p-10">
       <h1 className="text-lg font-bold">{"Admin Page"}</h1>
       <form className="flex w-full" onSubmit={search}>
-        <input type="text" className="w-full px-3" />
+        <input
+          type="text"
+          name="search"
+          className="w-full px-3 border-2"
+          placeholder="search student"
+          autoComplete="off"
+          autoCapitalize="off"
+        />
+        <button>Search</button>
       </form>
       <div className="flex py-1 w-full justify-between">
         <button>Name: </button>
         <button>Created Date</button>
       </div>
       <div className="">
-        {students.map((s) => (
+        {active?.map((s) => (
           <button
             key={s.id}
             className="flex py-3 hover:bg-gray-200 w-full justify-between px-1 font-bold"
